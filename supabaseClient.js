519bcf983d1e2dacc6fce6b79f4ae46c807f1c0b
samuelalencar-ai <samuel.alencar@escola.pr.gov.br>
2026-03-17 09:56:08 -0300

    \zvbn

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,9 +1,10 @@
+
 // Importa a função de criação do client Supabase via CDN
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 // Cria o client com a URL e a chave fornecidas
 export const supabase = createClient(
-  'https://qhywmeuqwquhlaryripu.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFoeXdtZXVxd3F1aGxhcnlyaXB1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI3MTA4MjEsImV4cCI6MjA4ODI4NjgyMX0.iN88OZWQg8MWeee1VU15g2bFMAJ-AC0v85w_8kH_ouc'
+  'SUPABASE_URL',
+  'SUPABASE_ANON_KEY'
 )
     
